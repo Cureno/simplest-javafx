f9bed7724af4abffbcb27ee70171bfa7a86a19d3
Cureno <14926132+Cureno@users.noreply.github.com>
2019-05-29 02:50:40 +0200

    A working JavaFX application written in Clojure.

--- src/simplest_javafx/core.clj
+++ src/simplest_javafx/core.clj
@@ -1,7 +1,14 @@
 (ns simplest-javafx.core
-  (:gen-class))
+  (:gen-class :extends javafx.application.Application)
+  (:import (javafx.application Application)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ; note that there is an underscore _ instead of a dash -
+  (Application/launch simplest_javafx.core args)) 
+
+(defn -start
+  "overrides javafx.application.Application's start(Stage primaryStage) method"
+  [this stage]
+  (.show stage))
