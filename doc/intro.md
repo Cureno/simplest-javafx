4a61661c105c64091fa8205c7d7a3c189e3eae06
Cureno <14926132+Cureno@users.noreply.github.com>
2019-05-29 02:37:14 +0200

    Initial commit, generated by the command "lein new app simplest-javafx"

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simplest-javafx
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/simplest_javafx/core_test.clj
@@ -0,0 +1,7 @@
+(ns simplest-javafx.core-test
+  (:require [clojure.test :refer :all]
+            [simplest-javafx.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
